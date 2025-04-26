039618b557e60ff113ca6b9e4ef9bcad2b25c184
Igor Britsky <igor.britsky@lge.com>
2025-04-27 00:50:32 +0300

    feat: add read stream implementation

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,12 @@
-const read = async () => {
-    // Write your code here 
+import { stdout } from 'node:process';
+import {
+    createReadStream,
+  } from 'node:fs';
+
+  const read = async () => {
+    const streamRead = createReadStream("./src/streams/files/fileToRead.txt");
+
+    streamRead.pipe(process.stdout);
 };
 
 await read();

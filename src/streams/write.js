cbe510cc8975a8512d3ec07d3596b754261c88b5
Igor Britsky <igor.britsky@lge.com>
2025-04-27 00:59:15 +0300

    feat: add the stream:write task

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,14 @@
-const write = async () => {
-    // Write your code here 
+import { stdin } from 'node:process';
+import {
+    createWriteStream,
+  } from 'node:fs';
+
+  const write = async () => {
+    const filename = "./src/streams/files/fileToWrite.txt";
+    const streamWrite = createWriteStream(filename);
+
+    console.log("Enter test to save into the " + filename + ". Press Enter and Ctrl+C to finish");
+    stdin.pipe(streamWrite);
 };
 
 await write();

682bda20cecb0f5e131492d2a8d53fc5acf92199
Igor Britsky <igor.britsky@lge.com>
2025-04-28 23:28:23 +0300

    feat: add the ZLib decompression

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,35 @@
+import fs, {
+
+    createReadStream,
+    createWriteStream,
+} from 'node:fs';
+
+import { createGunzip } from 'node:zlib';
+import { pipeline } from 'node:stream/promises';
+
+
 const decompress = async () => {
-    // Write your code here 
+    const inputArchive = "./src/zip/files/archive.gz";
+    const outputFile = "./src/zip/files/fileToCompress.txt";
+
+    try {
+        if (!fs.existsSync(inputArchive)) {
+            throw new Error("Has no input file");
+        }
+
+        if (fs.existsSync(outputFile)) {
+            throw new Error("Output archieve already present on the file system");
+        }
+
+        const unpackedStream = createGunzip();
+        const inputStream = createReadStream(inputArchive);
+        const outputStream = createWriteStream(outputFile);
+
+        await pipeline(inputStream, unpackedStream, outputStream);
+    } catch (err) {
+        console.error(err);
+    }
+
 };
 
 await decompress();

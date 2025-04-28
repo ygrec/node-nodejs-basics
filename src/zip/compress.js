6d52e3f8c9358e5bbc26cd422fe2bd82d15ffdae
Igor Britsky <igor.britsky@lge.com>
2025-04-28 23:28:06 +0300

    feat: add the Zlib compression

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,34 @@
+import fs, {
+    createReadStream,
+    createWriteStream,
+  } from 'node:fs';
+
+  import { createGzip } from 'node:zlib';
+  import { pipeline } from 'node:stream/promises';
+    
+  
 const compress = async () => {
-    // Write your code here 
+    const inputFile = "./src/zip/files/fileToCompress.txt";
+    const outputArchive = "./src/zip/files/archive.gz";
+    
+    try {
+        if (!fs.existsSync(inputFile)) {
+          throw new Error("Has no input file");
+        }
+
+        if(fs.existsSync(outputArchive)) {
+            throw new Error("Output archieve already present on the file system");
+        }
+
+        const packedStream = createGzip();
+        const inputStream = createReadStream(inputFile);
+        const outputStream = createWriteStream(outputArchive);
+    
+        await pipeline(inputStream, packedStream, outputStream);
+    
+      } catch (err) {
+        console.error(err);
+      }
 };
 
 await compress();

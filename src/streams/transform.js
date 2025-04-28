ff59e9e08612fd06f3183d337239e73f53997c91
Igor Britsky <igor.britsky@lge.com>
2025-04-28 22:27:10 +0300

    feat: add the transform implementation

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,27 @@
+import { stdin, stdout } from 'node:process';
+import {
+    Transform,
+} from 'node:stream';
+
+const streamTransform = new Transform({ decodeStrings: true,
+    transform(chunk, encoding, done) {
+        if(chunk.byteLength <= 2 ) {
+            process.stdin.destroy();
+            done();
+        }
+        else {
+            console.log("Origin  is " + chunk.toString());     
+            const reverseChunk = chunk.toString().split("").reverse().join("");
+            console.log("Reverse is " + reverseChunk);
+            this.push(reverseChunk);
+            done();
+        }
+    },
+});
+
 const transform = async () => {
-    // Write your code here 
+    stdin.pipe(streamTransform).pipe(stdout);
+    stdin.on('end', () => process.exit(exitCode));
 };
 
 await transform();

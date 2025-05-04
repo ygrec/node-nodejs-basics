5b256712983f1f5d01fbed80ff1a585b6db863c5
Igor Britsky <igor.britsky@lge.com>
2025-05-04 10:25:53 +0300

    feat: add the draft WorkerThread implementation.
    *Done after deadline for myself. Plz, don`t pay attention

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,21 @@
+import os from 'node:os';
+import path from 'path';
+import { Worker, MessageChannel } from 'worker_threads';
+
+
 const performCalculations = async () => {
-    // Write your code here
+
+    const workerScriptPath = import.meta.url;
+    const startComputationValue = 10;
+
+    console.log(os.cpus().length);
+
+    const worker = new Worker('./src/wt/worker.js', {workerData: {number: startComputationValue}} );
+    worker.on('message', (data) => {
+        console.log("data from main: " + data);
+    });
+
+    worker.postMessage(10);
 };
 
 await performCalculations();
--- src/wt/worker.js
+++ src/wt/worker.js
@@ -1,8 +1,15 @@
 // n should be received from main thread
+import { workerData, parentPort, MessagePort  } from 'worker_threads';
+
 const nthFibonacci = (n) => n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
 
 const sendResult = () => {
-    // This function sends result of nthFibonacci computations to main thread
+    console.log(workerData.number);
+
+    const result = nthFibonacci(10);
+    console.log("result from worker: " + result);
+    
+    parentPort.postMessage(result);
 };
 
 sendResult();

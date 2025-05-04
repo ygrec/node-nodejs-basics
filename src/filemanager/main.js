c6d025a7f7f33d085cd9e6bf0f054ed30d545b51
Igor Britsky <igor.britsky@lge.com>
2025-05-04 20:51:34 +0300

    feat: add the user detection and welcome/farewell messages

--- src/filemanager/main.js
+++ src/filemanager/main.js
@@ -1,30 +1,89 @@
-import { stdin } from 'node:process';
+import os from 'node:os';
+import { fileURLToPath } from 'node:url';
+import { dirname } from 'path';
+import { argv, env, stdin, stdout } from 'node:process';
+
+import { logMsg, logInfo, logDbg } from '../utils.js';
+
+var username = '';
+const greetingString = 'Welcome to the File Manager';
+
+
+function parseAgrsAndEnv() {
+  // username can be passed to the NPM and loaded to the ENV of script
+  const resultEnv = Object.entries(env)
+    .filter(([key]) => key.startsWith('npm_config_username'))
+    .map(([key, value]) => `${value}`);
+
+  if (resultEnv[0]) {
+    username = resultEnv[0];
+    logDbg('Found the arg, passed through the NPM: ' + os.EOL + resultEnv, 'debug');
+    return;
+  }
+
+  // username can be passed to the script via the CLI args or package.json
+  // node main.js --username User1
+  const args = argv.slice(2);
+  const result = args
+    .reduce((accumulator, nextValue, index) => {
+      const key = nextValue;
+      const value = args[index + 1];
+      if (index % 2 === 0) accumulator.push(`${key} is ${value}`);
+      return accumulator;
+    }, [])
+    .join(', ');
+
+  if(result) {
+    username = result;
+    logDbg('Node ARGS is: ' + result, 'debug');
+    return;
+  }
+
+  username = 'guest';
+}
+
+process.stdin.resume();
+
+// We are using this single function to handle multiple signals
+function exitHandler(signal) {
+  const farewellString = `Thank you for using File Manager, ${username}, goodbye!`;
+
+  logMsg(farewellString, 'important');
+  process.exit();
+}
+ 
+process.on('SIGINT', exitHandler);
 
 
 const startFileManager = async () => {
 
+  parseAgrsAndEnv();
+
+  logMsg(`${greetingString}, ${username}`, 'important');
+
+  let currentPath = dirname(fileURLToPath(import.meta.url));
+  logMsg("Current Path is " + currentPath + os.EOL + 'import as ' + import.meta.dirname,);
+
   stdin.on('data', (chunk) => {
-    console.log(`Received chunk ${chunk}`);
+    logDbg(`Received chunk ${chunk}`);
     const command = chunk.toString().trim().split(' ');
     switch (command[0]) {
       case 'up':
-        console.log('cmd UP parsed');
+        logDbg('cmd UP parsed');
         break;
       case 'cd':
-        console.log('cmd CD parsed');
+        logDbg('cmd CD parsed');
         break;
       case '.exit':
-        console.log('cmd EXIT parsed, close the filemanager');
-        stdin.destroy();
+        logDbg('cmd EXIT parsed, close the filemanager');
+        // stdin.destroy();
+        exitHandler();
         break;
       default:
-        console.log(`Unknown command \"${command[0]}\", please, try again!`);
+        logMsg(`Unknown command \"${command[0]}\", please, try again!`, 'important');
         break;
     }
-  })
-    .on('end', () => {
-      console.log("Good bye!");
-    });
+  });
 };
 
 await startFileManager();
